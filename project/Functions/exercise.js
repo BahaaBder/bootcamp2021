dc18298ff8e5bf409d7dee804f9bba1f438f752c
bahaabder <bahaa.bder97@gmail.com>
2021-04-20 17:16:44 +0300

    adding some code

--- project/Functions/exercise.js
+++ project/Functions/exercise.js
@@ -1,6 +1,32 @@
+//ex1
 const calcAge=function(x,y){
     return x-y
 }
 
 const age = calcAge(2017, 1989)
 console.log(age)
+// ex3
+let isEven=function(number){
+    if(number%2==0){
+        return true
+    }
+    else 
+    {
+        return false
+    }
+}
+console.log(isEven(6))
+
+//ex4
+let array=[1,2,2,3,4]
+const printOddnumbers=function(arr){
+    for(let i of arr){
+        if(!isEven(i)){
+            console.log(i)
+        }
+    }
+}
+
+printOddnumbers(array)
+//ex5
+
